6595a79da6f342df5140be187a7815e1eb3a6655
tanishqjaiswal28-sudo <tanishqjaiswal28@gmail.com>
2026-08-15 00:23:22 +0530

    Step 10: check_winner

--- model.py
+++ model.py
@@ -108,8 +108,25 @@ def four_in_a_row_diagonal_up_right(board):
                 
     return 0
 
-# Step 10 - check_winner (not yet solved)
-# TODO: implement
+# Step 10 - check_winner
+def check_winner(board):
+    winner = four_in_a_row_horizontal(board)
+    if winner != 0:
+        return winner
+        
+    winner = four_in_a_row_vertical(board)
+    if winner != 0:
+        return winner
+        
+    winner = four_in_a_row_diagonal_up_right(board)
+    if winner != 0:
+        return winner
+        
+    winner = four_in_a_row_diagonal_down_right(board)
+    if winner != 0:
+        return winner
+        
+    return 0
 
 # Step 11 - board_is_full (not yet solved)
 # TODO: implement
